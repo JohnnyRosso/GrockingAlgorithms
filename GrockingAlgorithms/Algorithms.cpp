fb6036350b1680f61e1709c316b593aeff3e1387
Роман Иванов <kolos.russkiy@mail.ru>
2023-10-29 17:31:35 +0300

    BinarySearch_vol2

--- GrockingAlgorithms/Algorithms.cpp
+++ GrockingAlgorithms/Algorithms.cpp
@@ -6,40 +6,61 @@
 
 int main()
 {
-    std::cout << "Algorithms\n\n";
-    std::cout << "1.BinarySearch\n";
-    std::cout << "\n";
-    std::cout << "Choose an algorithm to test ";
-
-    int algorithmNumber;
-    std::cin >> algorithmNumber;
-
-    switch (algorithmNumber)
+    while (true)
     {
-    case 1: //BinarySearch
-        system("cls");
-        std::cout << "BinarySearch\n\n";
-        std::cout << "Enter the size of array ";
-        int size;
-        std::cin >> size;
-        std::vector<int> numbers(size);
-        std::cout << "Enter the array ";
-        
-        for (int i = 0; i < size; i++)
-            std::cin >> numbers[i];
+        std::cout << "Algorithms\n\n";
+        std::cout << "1.BinarySearch\n";
+        std::cout << "2.Exit\n";
+        std::cout << "\n";
+        std::cout << "Choose an option ";
 
-        std::sort(numbers.begin(), numbers.end());
-        std::cout << "Sorted array: ";
-        for (int i = 0; i < size; i++)
-            std::cout << numbers[i] << " ";
+        int algorithmNumber;
 
-        int value;
-        std::cout << "\nEnter the value to find ";
-        std::cin >> value;
+        std::cin >> algorithmNumber;
 
-        std::cout << "Index of value: " << BinarySearch(numbers, value);
+        switch (algorithmNumber)
+        {
+            case 1: //BinarySearch
+            {
+                system("cls");
+                std::cout << "BinarySearch\n\n";
+                std::cout << "Enter the size of array ";
+                int size;
+                std::cin >> size;
+                std::vector<int> numbers(size);
+
+                std::cout << "";
+
+                std::cout << "Enter the array ";
+
+                for (int i = 0; i < size; i++)
+                    std::cin >> numbers[i];
+
+                std::sort(numbers.begin(), numbers.end());
+                std::cout << "Sorted array: ";
+                for (int i = 0; i < size; i++)
+                    std::cout << numbers[i] << " ";
+
+                int value;
+                std::cout << "\nEnter the value to find ";
+                std::cin >> value;
+                std::cout << "\n";
+
+                std::vector<int> BinarySearchResult = BinarySearch(numbers, value);
+                std::cout << "Index of value: " << BinarySearchResult[0];
+                std::cout << "\n";
+                std::cout << "Amount of operations: " << BinarySearchResult[1];
+                std::cout << "\n\n";
+
+                system("pause");
+                system("cls");
+                break;
+            }
+
+            default:
+                return 0;
+        }
     }
-
 
     return 0;
 }
--- GrockingAlgorithms/BinarySearch.h
+++ GrockingAlgorithms/BinarySearch.h
@@ -3,16 +3,22 @@
 #include <vector>
 #include <algorithm>
 
-int BinarySearch(std::vector<int> numbers, int value)
+std::vector<int> BinarySearch(std::vector<int> numbers, int value)
 {
-    int left = 0, right = numbers.size() - 1;
+    int left = 0, right = numbers.size() - 1, operationCounter = 0;
+    std::vector<int> result(2);
 
     while (left <= right)
     {
+        operationCounter++;
         int mid = (left + right) / 2;
 
         if (value == numbers[mid])
-            return mid;
+        {
+            result[0] = mid;
+            result[1] = operationCounter;
+            return result;
+        }
 
         if (value > numbers[mid])
             left = mid + 1;
@@ -20,5 +26,7 @@ int BinarySearch(std::vector<int> numbers, int value)
         else
             right = mid - 1;
     }
-    return 0;
+    result[0] = -1;
+    result[1] = operationCounter;
+    return result;
 }
